885c797955020fbd447bf5b6bc51a2a346d53094
firyal-salsa <firsabilacahyadi@gmail.com>
2023-12-10 21:40:03 +0700

    fixed bug meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+const metadata = {
   title: "Firyal Yusriyah Salsabila",
   description: "a DevOps Engineer and Odoo Developer.",
 };
